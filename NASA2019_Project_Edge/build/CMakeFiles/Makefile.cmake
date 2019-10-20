78b94ead9cf1c803645bfadf50576dd3f6bfc720
Charles Lien <lienching@lienching-thinkpad>
2019-10-20 12:18:19 +0800

    Add execute file for edge

--- NASA2019_Project_Edge/build/CMakeFiles/Makefile.cmake
+++ NASA2019_Project_Edge/build/CMakeFiles/Makefile.cmake
@@ -44,4 +44,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/SR_visionrecovery.dir/DependInfo.cmake"
+  "CMakeFiles/RNN_dataimputation.dir/DependInfo.cmake"
   )
---
+++ NASA2019_Project_Edge/build/CMakeFiles/RNN_dataimputation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RNN_dataimputation.dir/src/TFLiteRNN.cpp.o"
+  "RNN_dataimputation.pdb"
+  "RNN_dataimputation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RNN_dataimputation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
